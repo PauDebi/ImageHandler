665ea7821370555ae8666ffe071d83bcc659a100
brezhy <127342001+brezhy@users.noreply.github.com>
2024-12-10 13:16:39 +0100

    Nuevo boton en ImageHandler para editar la imagen (no hace nada, de momento solo imprime un mensaje por consola para comprobar si funciona)

---
+++ src/main/java/com/imageHandler/ImageEditor.java
@@ -0,0 +1,9 @@
+package com.imageHandler;
+
+public class ImageEditor {
+    public void editImage() {
+        // Lógica para editar la imagen
+        System.out.println("Editando la imagen...");
+        // Aquí podrías abrir una nueva ventana, aplicar filtros, etc.
+    }
+}
